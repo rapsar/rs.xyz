65d26e2cdbf4ba242488d3b3dfa414ac144f99ed
Raphael <48169917+rapsar@users.noreply.github.com>
2023-04-25 23:10:38 -0400

    activated slider-publi
    commented out the workSlider function and replace slider by slider-publi

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -175,84 +175,85 @@ $( document ).ready(function() {
 
   }
 
-  // function workSlider() {
-  //
-  //   $('.slider--prev, .slider--next').click(function() {
-  //
-  //     var $this = $(this),
-  //     curLeft = $('.slider').find('.slider--item-left'),
-  //     curLeftPos = $('.slider').children().index(curLeft),
-  //     curCenter = $('.slider').find('.slider--item-center'),
-  //     curCenterPos = $('.slider').children().index(curCenter),
-  //     curRight = $('.slider').find('.slider--item-right'),
-  //     curRightPos = $('.slider').children().index(curRight),
-  //     totalWorks = $('.slider').children().length,
-  //     $left = $('.slider--item-left'),
-  //     $center = $('.slider--item-center'),
-  //     $right = $('.slider--item-right'),
-  //     $item = $('.slider--item');
-  //
-  //     $('.slider').animate({ opacity : 0 }, 400);
-  //
-  //     setTimeout(function(){
-  //
-  //       if ($this.hasClass('slider--next')) {
-  //         if (curLeftPos < totalWorks - 1 && curCenterPos < totalWorks - 1 && curRightPos < totalWorks - 1) {
-  //           $left.removeClass('slider--item-left').next().addClass('slider--item-left');
-  //           $center.removeClass('slider--item-center').next().addClass('slider--item-center');
-  //           $right.removeClass('slider--item-right').next().addClass('slider--item-right');
-  //         }
-  //         else {
-  //           if (curLeftPos === totalWorks - 1) {
-  //             $item.removeClass('slider--item-left').first().addClass('slider--item-left');
-  //             $center.removeClass('slider--item-center').next().addClass('slider--item-center');
-  //             $right.removeClass('slider--item-right').next().addClass('slider--item-right');
-  //           }
-  //           else if (curCenterPos === totalWorks - 1) {
-  //             $left.removeClass('slider--item-left').next().addClass('slider--item-left');
-  //             $item.removeClass('slider--item-center').first().addClass('slider--item-center');
-  //             $right.removeClass('slider--item-right').next().addClass('slider--item-right');
-  //           }
-  //           else {
-  //             $left.removeClass('slider--item-left').next().addClass('slider--item-left');
-  //             $center.removeClass('slider--item-center').next().addClass('slider--item-center');
-  //             $item.removeClass('slider--item-right').first().addClass('slider--item-right');
-  //           }
-  //         }
-  //       }
-  //       else {
-  //         if (curLeftPos !== 0 && curCenterPos !== 0 && curRightPos !== 0) {
-  //           $left.removeClass('slider--item-left').prev().addClass('slider--item-left');
-  //           $center.removeClass('slider--item-center').prev().addClass('slider--item-center');
-  //           $right.removeClass('slider--item-right').prev().addClass('slider--item-right');
-  //         }
-  //         else {
-  //           if (curLeftPos === 0) {
-  //             $item.removeClass('slider--item-left').last().addClass('slider--item-left');
-  //             $center.removeClass('slider--item-center').prev().addClass('slider--item-center');
-  //             $right.removeClass('slider--item-right').prev().addClass('slider--item-right');
-  //           }
-  //           else if (curCenterPos === 0) {
-  //             $left.removeClass('slider--item-left').prev().addClass('slider--item-left');
-  //             $item.removeClass('slider--item-center').last().addClass('slider--item-center');
-  //             $right.removeClass('slider--item-right').prev().addClass('slider--item-right');
-  //           }
-  //           else {
-  //             $left.removeClass('slider--item-left').prev().addClass('slider--item-left');
-  //             $center.removeClass('slider--item-center').prev().addClass('slider--item-center');
-  //             $item.removeClass('slider--item-right').last().addClass('slider--item-right');
-  //           }
-  //         }
-  //       }
-  //
-  //     }, 400);
-  //
-  //     $('.slider').animate({ opacity : 1 }, 400);
-  //
-  //   });
-  //
-  // }
-  //
+//changed to slider-publi
+  function workSliderPubli() {
+
+    $('.slider-publi--prev, .slider-publi--next').click(function() {
+
+      var $this = $(this),
+      curLeft = $('.slider-publi').find('.slider-publi--item-left'),
+      curLeftPos = $('.slider-publi').children().index(curLeft),
+      curCenter = $('.slider-publi').find('.slider-publi--item-center'),
+      curCenterPos = $('.slider-publi').children().index(curCenter),
+      curRight = $('.slider-publi').find('.slider-publi--item-right'),
+      curRightPos = $('.slider-publi').children().index(curRight),
+      totalWorks = $('.slider-publi').children().length,
+      $left = $('.slider-publi--item-left'),
+      $center = $('.slider-publi--item-center'),
+      $right = $('.slider-publi--item-right'),
+      $item = $('.slider-publi--item');
+
+      $('.slider-publi').animate({ opacity : 0 }, 400);
+
+      setTimeout(function(){
+
+        if ($this.hasClass('slider-publi--next')) {
+          if (curLeftPos < totalWorks - 1 && curCenterPos < totalWorks - 1 && curRightPos < totalWorks - 1) {
+            $left.removeClass('slider-publi--item-left').next().addClass('slider-publi--item-left');
+            $center.removeClass('slider-publi--item-center').next().addClass('slider-publi--item-center');
+            $right.removeClass('slider-publi--item-right').next().addClass('slider-publi--item-right');
+          }
+          else {
+            if (curLeftPos === totalWorks - 1) {
+              $item.removeClass('slider-publi--item-left').first().addClass('slider-publi--item-left');
+              $center.removeClass('slider-publi--item-center').next().addClass('slider-publi--item-center');
+              $right.removeClass('slider-publi--item-right').next().addClass('slider-publi--item-right');
+            }
+            else if (curCenterPos === totalWorks - 1) {
+              $left.removeClass('slider-publi--item-left').next().addClass('slider-publi--item-left');
+              $item.removeClass('slider-publi--item-center').first().addClass('slider-publi--item-center');
+              $right.removeClass('slider-publi--item-right').next().addClass('slider-publi--item-right');
+            }
+            else {
+              $left.removeClass('slider-publi--item-left').next().addClass('slider-publi--item-left');
+              $center.removeClass('slider-publi--item-center').next().addClass('slider-publi--item-center');
+              $item.removeClass('slider-publi--item-right').first().addClass('slider-publi--item-right');
+            }
+          }
+        }
+        else {
+          if (curLeftPos !== 0 && curCenterPos !== 0 && curRightPos !== 0) {
+            $left.removeClass('slider-publi--item-left').prev().addClass('slider-publi--item-left');
+            $center.removeClass('slider-publi--item-center').prev().addClass('slider-publi--item-center');
+            $right.removeClass('slider-publi--item-right').prev().addClass('slider-publi--item-right');
+          }
+          else {
+            if (curLeftPos === 0) {
+              $item.removeClass('slider-publi--item-left').last().addClass('slider-publi--item-left');
+              $center.removeClass('slider-publi--item-center').prev().addClass('slider-publi--item-center');
+              $right.removeClass('slider-publi--item-right').prev().addClass('slider-publi--item-right');
+            }
+            else if (curCenterPos === 0) {
+              $left.removeClass('slider-publi--item-left').prev().addClass('slider-publi--item-left');
+              $item.removeClass('slider-publi--item-center').last().addClass('slider-publi--item-center');
+              $right.removeClass('slider-publi--item-right').prev().addClass('slider-publi--item-right');
+            }
+            else {
+              $left.removeClass('slider-publi--item-left').prev().addClass('slider-publi--item-left');
+              $center.removeClass('slider-publi--item-center').prev().addClass('slider-publi--item-center');
+              $item.removeClass('slider-publi--item-right').last().addClass('slider-publi--item-right');
+            }
+          }
+        }
+
+      }, 400);
+
+      $('.slider-publi').animate({ opacity : 1 }, 400);
+
+    });
+
+  }
+
   // function transitionLabels() {
   //
   //   $('.work-request--information input').focusout(function(){
@@ -272,9 +273,9 @@ $( document ).ready(function() {
   //   });
   //
   // }
-  //
-  // outerNav();
-  // workSlider();
+
+  outerNav();
+  workSliderPubli();
   // transitionLabels();
 
   /////////////////////////////////////////////added RS
